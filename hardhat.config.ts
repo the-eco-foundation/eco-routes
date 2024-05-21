18bdc68a5207e9f1be504a5d97adef7e520dabe3
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2024-05-21 12:34:45 -0400

    update solidity and hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -10,7 +10,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: '0.8.25',
+        version: '0.8.26',
         settings: {
           viaIR: true,
           optimizer: {
