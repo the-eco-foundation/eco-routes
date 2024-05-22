b9d4da0290a626fea6e5c6c8f1a0525b56aac132
John Whitton <jwhitton@eco.com>
2024-05-22 09:52:52 -0700

    Update to get workiign hardhat issue 3385

--- test/IntentSource.spec.ts
+++ test/IntentSource.spec.ts
@@ -1,12 +1,13 @@
 import { SignerWithAddress } from '@nomicfoundation/hardhat-ethers/signers'
 import { expect } from 'chai'
-import { ethers } from 'hardhat'
+import hre from 'hardhat'
 import { TestERC20, IntentSource, TestProver } from '../typechain-types'
 import {
   time,
   loadFixture,
 } from '@nomicfoundation/hardhat-toolbox/network-helpers'
 import { DataHexString } from 'ethers/lib.commonjs/utils/data'
+const { ethers } = hre
 
 describe('Intent Source Test', (): void => {
   let source: IntentSource
@@ -53,155 +54,155 @@ describe('Intent Source Test', (): void => {
     }
   }
 
-//   async function setBalances() {
-//     await erc20.connect(owner).transfer(await solver.getAddress(), mintAmount)
-//   }
+  //   async function setBalances() {
+  //     await erc20.connect(owner).transfer(await solver.getAddress(), mintAmount)
+  //   }
 
-//   async function encodeTransfer(
-//     to: string,
-//     value: number,
-//   ): Promise<DataHexString> {
-//     // Contract ABIs
-//     const erc20ABI = ['function transfer(address to, uint256 value)']
-//     const abiInterface = new ethers.Interface(erc20ABI)
-//     const callData = abiInterface.encodeFunctionData('transfer', [to, value])
-//     return callData
-//   }
+  //   async function encodeTransfer(
+  //     to: string,
+  //     value: number,
+  //   ): Promise<DataHexString> {
+  //     // Contract ABIs
+  //     const erc20ABI = ['function transfer(address to, uint256 value)']
+  //     const abiInterface = new ethers.Interface(erc20ABI)
+  //     const callData = abiInterface.encodeFunctionData('transfer', [to, value])
+  //     return callData
+  //   }
 
-//   beforeEach(async (): Promise<void> => {
-//     ;({ inbox, erc20, owner, solver, dstAddr } =
-//       await loadFixture(deployInboxFixture))
+  //   beforeEach(async (): Promise<void> => {
+  //     ;({ inbox, erc20, owner, solver, dstAddr } =
+  //       await loadFixture(deployInboxFixture))
 
-//     // fund the solver
-//     await setBalances()
-//     erc20Address = await erc20.getAddress()
-//     calldata = await encodeTransfer(dstAddr.address, mintAmount)
-//     timeStamp = (await time.latest()) + timeDelta
-//     const abiCoder = ethers.AbiCoder.defaultAbiCoder()
-//     const encodedData = abiCoder.encode(
-//       ['uint256', 'address[]', 'bytes[]', 'uint256'],
-//       [nonce, [erc20Address], [calldata], timeStamp],
-//     )
-//     hash32 = ethers.keccak256(encodedData)
-//   })
+  //     // fund the solver
+  //     await setBalances()
+  //     erc20Address = await erc20.getAddress()
+  //     calldata = await encodeTransfer(dstAddr.address, mintAmount)
+  //     timeStamp = (await time.latest()) + timeDelta
+  //     const abiCoder = ethers.AbiCoder.defaultAbiCoder()
+  //     const encodedData = abiCoder.encode(
+  //       ['uint256', 'address[]', 'bytes[]', 'uint256'],
+  //       [nonce, [erc20Address], [calldata], timeStamp],
+  //     )
+  //     hash32 = ethers.keccak256(encodedData)
+  //   })
 
-//   describe('when the intent is invalid', () => {
-//     it('should revert if the timestamp is expired', async () => {
-//       timeStamp -= 2 * timeDelta
-//       await expect(
-//         inbox.fulfill(
-//           nonce,
-//           [erc20Address],
-//           [calldata],
-//           timeStamp,
-//           dstAddr.address,
-//         ),
-//       ).to.be.revertedWithCustomError(inbox, 'IntentExpired')
-//     })
+  //   describe('when the intent is invalid', () => {
+  //     it('should revert if the timestamp is expired', async () => {
+  //       timeStamp -= 2 * timeDelta
+  //       await expect(
+  //         inbox.fulfill(
+  //           nonce,
+  //           [erc20Address],
+  //           [calldata],
+  //           timeStamp,
+  //           dstAddr.address,
+  //         ),
+  //       ).to.be.revertedWithCustomError(inbox, 'IntentExpired')
+  //     })
 
-//     it('should revert if the data is invalid', async () => {
-//       // unequal length of addresses and calldata
-//       await expect(
-//         inbox.fulfill(
-//           nonce,
-//           [erc20Address],
-//           [calldata, calldata],
-//           timeStamp,
-//           dstAddr.address,
-//         ),
-//       ).to.be.revertedWithCustomError(inbox, 'InvalidData')
-//       // empty addresses
-//       await expect(
-//         inbox.fulfill(
-//           nonce,
-//           [],
-//           [calldata, calldata],
-//           timeStamp,
-//           dstAddr.address,
-//         ),
-//       ).to.be.revertedWithCustomError(inbox, 'InvalidData')
-//     })
-//   })
+  //     it('should revert if the data is invalid', async () => {
+  //       // unequal length of addresses and calldata
+  //       await expect(
+  //         inbox.fulfill(
+  //           nonce,
+  //           [erc20Address],
+  //           [calldata, calldata],
+  //           timeStamp,
+  //           dstAddr.address,
+  //         ),
+  //       ).to.be.revertedWithCustomError(inbox, 'InvalidData')
+  //       // empty addresses
+  //       await expect(
+  //         inbox.fulfill(
+  //           nonce,
+  //           [],
+  //           [calldata, calldata],
+  //           timeStamp,
+  //           dstAddr.address,
+  //         ),
+  //       ).to.be.revertedWithCustomError(inbox, 'InvalidData')
+  //     })
+  //   })
 
-//   describe('when the intent is valid', () => {
-//     it('should revert if the call fails', async () => {
-//       await expect(
-//         inbox.fulfill(
-//           nonce,
-//           [erc20Address],
-//           [calldata],
-//           timeStamp,
-//           dstAddr.address,
-//         ),
-//       ).to.be.revertedWithCustomError(inbox, 'IntentCallFailed')
-//     })
+  //   describe('when the intent is valid', () => {
+  //     it('should revert if the call fails', async () => {
+  //       await expect(
+  //         inbox.fulfill(
+  //           nonce,
+  //           [erc20Address],
+  //           [calldata],
+  //           timeStamp,
+  //           dstAddr.address,
+  //         ),
+  //       ).to.be.revertedWithCustomError(inbox, 'IntentCallFailed')
+  //     })
 
-//     it('should succeed', async () => {
-//       expect(await inbox.fulfilled(nonce)).to.be.deep.equal([
-//         ethers.ZeroHash,
-//         ethers.ZeroAddress,
-//       ])
-//       expect(await erc20.balanceOf(solver.address)).to.equal(mintAmount)
-//       expect(await erc20.balanceOf(dstAddr.address)).to.equal(0)
+  //     it('should succeed', async () => {
+  //       expect(await inbox.fulfilled(nonce)).to.be.deep.equal([
+  //         ethers.ZeroHash,
+  //         ethers.ZeroAddress,
+  //       ])
+  //       expect(await erc20.balanceOf(solver.address)).to.equal(mintAmount)
+  //       expect(await erc20.balanceOf(dstAddr.address)).to.equal(0)
 
-//       // transfer the tokens to the inbox so it can process the transaction
-//       await erc20.connect(solver).transfer(await inbox.getAddress(), mintAmount)
+  //       // transfer the tokens to the inbox so it can process the transaction
+  //       await erc20.connect(solver).transfer(await inbox.getAddress(), mintAmount)
 
-//       // should emit an event
-//       await expect(
-//         inbox
-//           .connect(solver)
-//           .fulfill(
-//             nonce,
-//             [erc20Address],
-//             [calldata],
-//             timeStamp,
-//             dstAddr.address,
-//           ),
-//       )
-//         .to.emit(inbox, 'Fulfillment')
-//         .withArgs(nonce)
-//       // should update the fulfilled hash
-//       expect(await inbox.fulfilled(nonce)).to.be.deep.equal([
-//         hash32,
-//         dstAddr.address,
-//       ])
+  //       // should emit an event
+  //       await expect(
+  //         inbox
+  //           .connect(solver)
+  //           .fulfill(
+  //             nonce,
+  //             [erc20Address],
+  //             [calldata],
+  //             timeStamp,
+  //             dstAddr.address,
+  //           ),
+  //       )
+  //         .to.emit(inbox, 'Fulfillment')
+  //         .withArgs(nonce)
+  //       // should update the fulfilled hash
+  //       expect(await inbox.fulfilled(nonce)).to.be.deep.equal([
+  //         hash32,
+  //         dstAddr.address,
+  //       ])
 
-//       // check balances
-//       expect(await erc20.balanceOf(solver.address)).to.equal(0)
-//       expect(await erc20.balanceOf(dstAddr.address)).to.equal(mintAmount)
-//     })
+  //       // check balances
+  //       expect(await erc20.balanceOf(solver.address)).to.equal(0)
+  //       expect(await erc20.balanceOf(dstAddr.address)).to.equal(mintAmount)
+  //     })
 
-//     it('should revert if the intent has already been fulfilled', async () => {
-//       // transfer the tokens to the inbox so it can process the transaction
-//       await erc20.connect(solver).transfer(await inbox.getAddress(), mintAmount)
+  //     it('should revert if the intent has already been fulfilled', async () => {
+  //       // transfer the tokens to the inbox so it can process the transaction
+  //       await erc20.connect(solver).transfer(await inbox.getAddress(), mintAmount)
 
-//       // should emit an event
-//       await expect(
-//         inbox
-//           .connect(solver)
-//           .fulfill(
-//             nonce,
-//             [erc20Address],
-//             [calldata],
-//             timeStamp,
-//             dstAddr.address,
-//           ),
-//       )
-//         .to.emit(inbox, 'Fulfillment')
-//         .withArgs(nonce)
-//       // should revert
-//       await expect(
-//         inbox
-//           .connect(solver)
-//           .fulfill(
-//             nonce,
-//             [erc20Address],
-//             [calldata],
-//             timeStamp,
-//             dstAddr.address,
-//           ),
-//       ).to.be.revertedWithCustomError(inbox, 'IntentAlreadyFulfilled')
-//     })
-//   })
+  //       // should emit an event
+  //       await expect(
+  //         inbox
+  //           .connect(solver)
+  //           .fulfill(
+  //             nonce,
+  //             [erc20Address],
+  //             [calldata],
+  //             timeStamp,
+  //             dstAddr.address,
+  //           ),
+  //       )
+  //         .to.emit(inbox, 'Fulfillment')
+  //         .withArgs(nonce)
+  //       // should revert
+  //       await expect(
+  //         inbox
+  //           .connect(solver)
+  //           .fulfill(
+  //             nonce,
+  //             [erc20Address],
+  //             [calldata],
+  //             timeStamp,
+  //             dstAddr.address,
+  //           ),
+  //       ).to.be.revertedWithCustomError(inbox, 'IntentAlreadyFulfilled')
+  //     })
+  //   })
 })
