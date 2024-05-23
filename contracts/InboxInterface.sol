705146917f4fc15066caeec0625c0e2d55765533
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2024-05-23 13:03:26 -0400

    Adding inbox contract (#2)
    * adding inbox contract and interface,
    setting up tests,
    updating package with new deps and some clean up
    updating hardhat configs
    adding github ci flow
    
    * update solidity and hardhat
    
    * adding skeleton tests
    
    * adding tests
    updating to use nonce as key
    
    * space
    
    * nonce to bytes32, remove data validation update tests

---
+++ contracts/InboxInterface.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+interface InboxInterface {
+    /**
+     * This function is the main entry point for fulfilling an intent. It validates that the hash is the hash of the other parameters.
+     * It then calls the addresses with the calldata, and if successful marks the intent as fulfilled and emits an event.
+     *
+     * @param _nonce The nonce of the calldata. Composed of the hash on the src chain of a global nonce & chainID
+     * @param _targets The addresses to call
+     * @param _datas The calldata to call
+     * @param _expireTimestamp The timestamp at which the intent expires
+     * @param _claimer The address who can claim the reward on the src chain. Not part of the hash
+     * @return results The results of the calls as an array of bytes
+     */
+    function fulfill(
+        bytes32 _nonce,
+        address[] calldata _targets,
+        bytes[] calldata _datas,
+        uint256 _expireTimestamp,
+        address _claimer
+    ) external returns (bytes[] memory);
+
+    // Event emitted when an intent is succesfully fulfilled
+    event Fulfillment(bytes32 indexed _nonce);
+    
+    // Event emitted when the intent can no longer be fulfilled because its timestamp has expired
+    error IntentExpired();
+    
+    // Event emitted when the intent has already been fulfilled
+    error IntentAlreadyFulfilled(bytes32 _nonce);
+    
+    // Event emitted when the intent call failed while itertating through the callAddresses
+    error IntentCallFailed(address _addr, bytes _data, bytes _returnData);
+}
---
+++ contracts/test/ERC20Test.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract ERC20Test is ERC20 {
+    constructor(
+        string memory name_,
+        string memory symbol_,
+        uint256 amount
+    ) ERC20(name_, symbol_) {
+        _mint(msg.sender, amount * 1 ether);
+    }
+}
