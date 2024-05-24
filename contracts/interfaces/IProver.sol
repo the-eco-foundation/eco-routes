511c730589ee1914e972032d80226074aee833d1
Albert Brown <albert@eco.com>
2024-05-24 11:57:52 -0700

    Eco 1848 (#3)
    * add OP contracts
    
    * add via-ir
    
    * prover updated for OP npm lib and cleaned up helper/test functions
    
    * general test contracts and a placeholder test
    
    * actually correct op package
    
    * undo a bunch of stupid changes I made cuz I had the wrong library
    
    * test folder isn't pluralized
    
    * ported over deployment util
    
    * no need to gate the sender in the mock l1block
    
    * tests ported
    
    * added some extra require strings
    
    * aaind prove generator
    
    * update and disable tests on prover for now
    
    ---------
    
    Co-authored-by: Albert Brown <albertbrown@Bobingy.local>
    Co-authored-by: Stoyan Dimitrov <s.dimitrov19@gmail.com>

---
+++ contracts/interfaces/IProver.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+interface IProver {
+    function NONCE_PACKING() external view returns (uint16);
+
+    function L2_OUTPUT_SLOT_NUMBER() external view returns (uint256);
+
+    function L2_OUTPUT_ROOT_VERSION_NUMBER() external view returns (uint256);
+
+    function L1_OUTPUT_ORACLE_ADDRESS() external view returns (address);
+
+    function l1BlockhashOracle() external view returns (address);
+
+    // mapping from l1 world state root hashes to block numbers they correspond to
+    function provenL1States(bytes32) external view returns (uint256);
+
+    function provenL2States(bytes32) external view returns (uint256);
+    
+    function provenIntents(bytes32) external view returns (address);
+
+    // useful helper function but should probably be removed
+    function rlpEncodeDataLibList(
+        bytes[] memory dataList
+    ) external pure returns (bytes memory);
+
+    function proveL1WorldState(
+        bytes calldata rlpEncodedL1BlockData
+    ) external;
+
+    function proveOutputRoot(
+        bytes32 l2WorldStateRoot,
+        bytes32 l2MessagePasserStateRoot,
+        bytes32 l2LatestBlockHash,
+        uint256 l2OutputIndex,
+        bytes[] calldata l1StorageProof,
+        bytes calldata rlpEncodedOutputOracleData,
+        bytes[] calldata l1AccountProof,
+        bytes32 l1WorldStateRoot
+    ) external;
+
+    function proveIntent(
+        address claimant,
+        address inboxContract,
+        bytes32 intentHash,
+        uint256 intentOutputIndex,
+        bytes[] calldata l2StorageProof,
+        bytes calldata rlpEncodedInboxData,
+        bytes[] calldata l2AccountProof,
+        bytes32 l2WorldStateRoot
+    ) external;
+}
