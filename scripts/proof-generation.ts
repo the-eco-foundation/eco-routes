2b316d71c92d04bdf2f9774d488dd08436005f0f
Nishaad <nknavkal@gmail.com>
2024-05-30 19:43:23 -0500

    got output root

--- scripts/proof-generation.ts
+++ scripts/proof-generation.ts
@@ -1,8 +1,9 @@
 import hre from 'hardhat'
 import * as L2OutputArtifact from '@eth-optimism/contracts-bedrock/forge-artifacts/L2OutputOracle.sol/L2OutputOracle.json'
-import { AlchemyProvider, Wallet, Signer } from 'ethers'
+import { AlchemyProvider, Wallet, Signer, hexlify } from 'ethers'
+import { toBytes } from 'viem'
 
-const PRIVATE_KEY = process.env.DEPLOY_PRIVATE_KEY || ''
+const PRIVATE_KEY = process.env.PRIVATE_KEY || ''
 const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY || ''
 const L1_NETWORK = 'sepolia'
 const L1Provider = new AlchemyProvider(L1_NETWORK, ALCHEMY_API_KEY)
@@ -10,9 +11,6 @@ const L1signer: Signer = new Wallet(PRIVATE_KEY, L1Provider)
 const L2_NETWORK = 'base-sepolia'
 const L2Provider = new AlchemyProvider(L2_NETWORK, ALCHEMY_API_KEY)
 const L2signer: Signer = new Wallet(PRIVATE_KEY, L2Provider)
-
-console.log(PRIVATE_KEY)
-console.log(ALCHEMY_API_KEY)
 
 const baseOutputContractAddress = '0x84457ca9D0163FbC4bbfe4Dfbb20ba46e48DF254' // sepolia address
 
@@ -35,8 +33,7 @@ async function main() {
   // hre.changeNetwork(L2_NETWORK)
   const txDetails = await L2Provider.getTransaction(txToProve)
   // console.log(hre.ethers.provider)
-  console.log(txDetails)
-  const txBlock = txDetails.blockNumber
+  const txBlock = txDetails!.blockNumber
 
   // await hre.changeNetwork(L1_NETWORK)
   // console.log(hre.ethers.provider)
@@ -45,29 +42,21 @@ async function main() {
     L2OutputArtifact,
     L1signer,
   ).attach(baseOutputContractAddress)
-  console.log('here now')
   // console.log(L1signer)
   // console.log(baseOutputContract)
   const outputIndex = await baseOutputContract.getL2OutputIndexAfter(txBlock)
-  console.log(outputIndex)
   const outputData = await baseOutputContract.getL2OutputAfter(txBlock)
-  console.log('outputData: %s', outputData)
-  // console.log(JSON.stringify(outputData))
-  // const l2EndBatchBlock = outputData.l2BlockNumber.toHexString()
-  const l2EndBatchBlock = outputData.l2BlockNumber
+  const l2EndBatchBlock = hexlify(toBytes(outputData.l2BlockNumber))
   console.log(l2EndBatchBlock)
+
   // eslint-disable-next-line no-unused-vars
   const outputRoot = outputData.outputRoot
   console.log(outputRoot)
 
-  await hre.changeNetwork(L2_NETWORK)
   const l2OutputStorageRoot = (
-    await hre.ethers.provider.send('eth_getBlockByNumber', [
-      l2EndBatchBlock,
-      false,
-    ])
+    await L2Provider.send('eth_getBlockByNumber', [l2EndBatchBlock, false])
   ).stateRoot
-  const proof = await hre.ethers.provider.send('eth_getProof', [
+  const proof = await L2Provider.send('eth_getProof', [
     inboxContract,
     [storageSlot],
     l2EndBatchBlock,
