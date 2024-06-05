0feb6b576ef175df80e789fd204c891085d5d04f
Nishaad <nknavkal@gmail.com>
2024-06-05 15:05:22 -0500

    mostly im not sure about this l1StorageProof input now, i cannot find it

--- scripts/proveOutputRoot.ts
+++ scripts/proveOutputRoot.ts
@@ -13,7 +13,7 @@ const baseRPCURL = 'https://base-sepolia.g.alchemy.com/v2/'
 const opRPCURL = 'https://op-sepolia.g.alchemy.com/v2/'
 const proverAddress = '0xBA820f11f874D39d8bc6097F051Fc7A238b62f0e'
 const L2OutputOracleAddress = '0x84457ca9D0163FbC4bbfe4Dfbb20ba46e48DF254'
-const L1SubmissionBlock = numberToHex(5897036)
+const L1SubmissionBlock = numberToHex(5903085)
 
 const L1WorldStateRoot = ''
 const batchIndex = ''
@@ -23,14 +23,23 @@ async function proveOutputRoot() {
   const opProvider = ethers.getDefaultProvider(opRPCURL + apikey)
   const L1Provider = ethers.getDefaultProvider(L1RPCURL + apikey)
 
+  const baseIntentFulfillBlock = numberToHex(9934282)
+
+  const L2toL1MessagePasser = '0x4200000000000000000000000000000000000016'
+
   //   const block: Block = await L1Provider.send('eth_getBlockByNumber', [
   //     blockNumber,
   //     false,
   //   ])
-  const proofOutput = await L1Provider.send('eth_getProof', [
-    L2OutputOracleAddress,
+  //   const proofOutput = await L1Provider.send('eth_getProof', [
+  //     L2OutputOracleAddress,
+  //     [],
+  //     L1SubmissionBlock,
+  //   ])
+  const proofOutput = await baseProvider.send('eth_getProof', [
+    L2toL1MessagePasser,
     [],
-    L1SubmissionBlock,
+    baseIntentFulfillBlock,
   ])
   console.log(proofOutput)
 }
