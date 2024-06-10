b4aee7cfb52f847ef9cd77028229ceb7333df6b2
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2024-06-10 15:22:09 -0400

    adding initial test script

--- scripts/fulfill-intent.ts
+++ scripts/fulfill-intent.ts
@@ -5,11 +5,11 @@ import { encodeTransfer } from '../utils/encode'
 
 async function main() {
   const nonce = ethers.encodeBytes32String('0x987')
-  const targets: string[] = ['erc20Address']
-  const calldata: string[] = [await encodeTransfer('0xbD7897D8c7dB7016e67D82aEd8b7490AACcaF5AB', 0)]
+  const targets: string[] = ['0xAb1D243b07e99C91dE9E4B80DFc2B07a8332A2f7']
+  const calldata: string[] = [await encodeTransfer('0x982E148216E3Aa6B38f9D901eF578B5c06DD7502', 1)]
   const timeStamp: number = 0
-  const claimerAddress: string = '0x123'
-  const inboxContractAddress = ''
+  const claimerAddress: string = '0x982E148216E3Aa6B38f9D901eF578B5c06DD7502'
+  const inboxContractAddress = '0x3a7e440f1c95fD2Ff8f1f028701f821Ad0fa018a'
 
   const [signer] = await ethers.getSigners()
   console.log('Interacting with account:', signer.address)
