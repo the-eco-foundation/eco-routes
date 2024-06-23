f8a97078704515442ef955eab53ef692f04fd8df
John Whitton <jwhitton@eco.com>
2024-06-22 17:18:17 -0700

    fix eslint issues

--- scripts/proof-generation.ts
+++ scripts/proof-generation.ts
@@ -1,5 +1,5 @@
 import hre from 'hardhat'
-import * as L2OutputArtifact from '@eth-optimism/contracts-bedrock/forge-artifacts/L2OutputOracle.sol/L2OutputOracle.json'
+// import * as L2OutputArtifact from '@eth-optimism/contracts-bedrock/forge-artifacts/L2OutputOracle.sol/L2OutputOracle.json'
 
 // const L1_NETWORK = 'sepolia'
 const L2_NETWORK = 'baseSepolia'
@@ -7,20 +7,20 @@ const L2_NETWORK = 'baseSepolia'
 // this is the address on sepolia (L1) where Base Sepolia's l2 state is posted --> L2 output oracle
 // const baseOutputContractAddress = '0x84457ca9D0163FbC4bbfe4Dfbb20ba46e48DF254' // sepolia address
 
-const txToProve =
-  '0x60a200bc0d29f1fe6e7c64a51f48d417a1a8d76c5ed7740e03207d46ecf193ee'
+// const txToProve =
+//   '0x60a200bc0d29f1fe6e7c64a51f48d417a1a8d76c5ed7740e03207d46ecf193ee'
 // const inboxContract = '0xa506283526A6948619Ac101f0ee7d21a86FcBEDA'
-const intentHash =
-  '0xaac8c197b419c8be5545949d5a1a6dc3514dd018dabd603f0e3c9006dec55105'
+// const intentHash =
+//   '0xaac8c197b419c8be5545949d5a1a6dc3514dd018dabd603f0e3c9006dec55105'
 // const storageSlot = hre.ethers.solidityPackedKeccak256(
-  ['bytes'],
-  [
-    hre.ethers.AbiCoder.defaultAbiCoder().encode(
-      ['bytes32', 'uint256'],
-      [intentHash, 0],
-    ),
-  ],
-)
+//   ['bytes'],
+//   [
+//     hre.ethers.AbiCoder.defaultAbiCoder().encode(
+//       ['bytes32', 'uint256'],
+//       [intentHash, 0],
+//     ),
+//   ],
+// )
 
 async function main() {
   hre.changeNetwork(L2_NETWORK)
