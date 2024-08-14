4cd49681656b319c63c771825ee71d9143750d75
Nishaad <nknavkal@gmail.com>
2024-08-14 12:49:46 -0400

    baseprover

--- contracts/interfaces/BaseProver.sol
+++ contracts/interfaces/BaseProver.sol
@@ -1,7 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
 
-interface BaseProver {
-    
-    function provenBy(bytes32 _hash) returns (address _claimant);
+abstract contract BaseProver {
+    mapping(bytes32 => address) provenBy;
 }
