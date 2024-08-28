83baca9ffb45e29db716a07721479be674b65a5a
John Whitton <jwhitton@eco.com>
2024-08-28 08:12:18 -0700

    Remove Testnet Scripts

--- scripts/mainnet/deploy.ts
+++ scripts/mainnet/deploy.ts
@@ -4,7 +4,7 @@ import { setTimeout } from 'timers/promises'
 // import { getAddress } from 'ethers'
 // import c from '../config/testnet/config'
 // import networks from '../config/testnet/config';
-import { networks, actors } from '../../config/mainnet/config'
+import { networks } from '../../config/mainnet/config'
 
 const networkName = network.name
 console.log('Deploying to Network: ', network.name)
