68e015013be76c352b338301e1c6acd701e8ed25
Nishaad <nknavkal@gmail.com>
2024-10-29 17:37:03 -0400

    done

--- scripts/mainnet/deploy.ts
+++ scripts/mainnet/deploy.ts
@@ -8,31 +8,30 @@ console.log('Deploying to Network: ', network.name)
 let deployNetwork: any
 let counter: number = 0
 let minimumDuration: number = 0
+let localGasLimit: number = 0
 switch (networkName) {
   case 'base':
-    counter = networks.base.intentSource.counter
-    minimumDuration = networks.base.intentSource.minimumDuration
     deployNetwork = networks.base
     break
   case 'optimism':
-    counter = networks.optimism.intentSource.counter
-    minimumDuration = networks.optimism.intentSource.minimumDuration
     deployNetwork = networks.optimism
     break
   case 'helix':
-    counter = networks.helix.intentSource.counter
-    minimumDuration = networks.helix.intentSource.minimumDuration
     deployNetwork = networks.helix
     break
+  case 'arbitrum':
+    deployNetwork = networks.arbitrum
+    break
+  case 'mantle':
+    deployNetwork = networks.mantle
 }
-console.log('Counter: ', counter)
 const initialSalt: string = 'HANDOFF0'
 // const initialSalt: string = 'PROD'
 
-let proverAddress = ''
-let intentSourceAddress = ''
-let inboxAddress = ''
-let hyperProverAddress = ''
+let proverAddress: string = ''
+let intentSourceAddress: string = '0xa6B316239015DFceAC5bc9c19092A9B6f59ed905'
+let inboxAddress: string = '0xfB853672cE99D9ff0a7DE444bEE1FB2C212D65c0'
+let hyperProverAddress: string = '0xB1017F865c6306319C65266158979278F7f50118'
 const isSolvingPublic = initialSalt !== 'PROD'
 console.log(
   `Deploying with salt: ethers.keccak256(ethers.toUtf8bytes(${initialSalt})`,
@@ -51,6 +50,10 @@ async function main() {
     'Deployer',
     '0xfc91Ac2e87Cc661B674DAcF0fB443a5bA5bcD0a3',
   )
+  localGasLimit = deployNetwork.gasLimit
+  counter = deployNetwork.intentSource.counter
+  minimumDuration = deployNetwork.intentSource.minimumDuration
+  console.log('localGasLimit:', localGasLimit)
 
   console.log(`**************************************************`)
   const baseChainConfiguration = {
@@ -85,6 +88,27 @@ async function main() {
       outputRootVersionNumber: networks.helix.proving.outputRootVersionNumber, // outputRootVersionNumber
     },
   }
+  //   const arbitrumChainConfiguration = {
+  //     chainId: networks.arbitrum.chainId, // chainId
+  //     chainConfiguration: {
+  //       provingMechanism: networks.arbitrum.proving.mechanism, // provingMechanism
+  //       settlementChainId: networks.arbitrum.proving.settlementChain.id, // settlementChainId
+  //       settlementContract: networks.arbitrum.proving.settlementChain.contract, // settlementContract e.g DisputGameFactory or L2OutputOracle.
+  //       blockhashOracle: networks.arbitrum.proving.l1BlockAddress, // blockhashOracle
+  //       outputRootVersionNumber:
+  //         networks.arbitrum.proving.outputRootVersionNumber, // outputRootVersionNumber
+  //     },
+  //   }
+  const mantleChainConfiguration = {
+    chainId: networks.mantle.chainId, // chainId
+    chainConfiguration: {
+      provingMechanism: networks.mantle.proving.mechanism, // provingMechanism
+      settlementChainId: networks.mantle.proving.settlementChain.id, // settlementChainId
+      settlementContract: networks.mantle.proving.settlementChain.contract, // settlementContract e.g DisputGameFactory or L2OutputOracle.
+      blockhashOracle: networks.mantle.proving.l1BlockAddress, // blockhashOracle
+      outputRootVersionNumber: networks.mantle.proving.outputRootVersionNumber, // outputRootVersionNumber
+    },
+  }
   let receipt
 
   if (proverAddress === '') {
@@ -93,9 +117,11 @@ async function main() {
       baseChainConfiguration,
       optimismChainConfiguration,
       helixChainConfiguration,
+      //   arbitrumChainConfiguration,
+      mantleChainConfiguration,
     ])
     receipt = await singletonDeployer.deploy(proverTx.data, salt, {
-      gasLimit: 5000000,
+      gasLimit: localGasLimit,
     })
     proverAddress = (
       await singletonDeployer.queryFilter(
@@ -113,7 +139,7 @@ async function main() {
       counter,
     )
     receipt = await singletonDeployer.deploy(intentSourceTx.data, salt, {
-      gasLimit: 5000000,
+      gasLimit: localGasLimit / 2,
     })
     intentSourceAddress = (
       await singletonDeployer.queryFilter(
@@ -123,7 +149,6 @@ async function main() {
     )[0].args.addr
   }
   console.log('intentSource deployed to:', intentSourceAddress)
-
   if (inboxAddress === '') {
     const inboxFactory = await ethers.getContractFactory('Inbox')
 
@@ -133,7 +158,7 @@ async function main() {
       [actors.solver],
     )
     receipt = await singletonDeployer.deploy(inboxTx.data, salt, {
-      gasLimit: 3000000,
+      gasLimit: localGasLimit / 2,
     })
     await receipt.wait()
     inboxAddress = (
@@ -166,8 +191,9 @@ async function main() {
     )
 
     receipt = await singletonDeployer.deploy(hyperProverTx.data, salt, {
-      gasLimit: 1000000,
+      gasLimit: localGasLimit / 4,
     })
+    await receipt.wait()
     console.log('hyperProver deployed')
 
     hyperProverAddress = (
@@ -193,6 +219,8 @@ async function main() {
             baseChainConfiguration,
             optimismChainConfiguration,
             helixChainConfiguration,
+            // arbitrumChainConfiguration,
+            mantleChainConfiguration,
           ],
         ],
       })
