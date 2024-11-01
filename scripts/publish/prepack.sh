7590778a12ee0d528cd366a3bbd3f4b605c666da
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2024-10-31 21:37:22 -0400

    add

--- scripts/publish/prepack.sh
+++ scripts/publish/prepack.sh
@@ -16,14 +16,15 @@ env COMPILE_MODE=production npm run build
 
 mkdirp build/src/abi/contracts
 mkdirp build/src/abi/interfaces
-
+echo "mkdir"
 cp artifacts/contracts/**/*.json build/src/abi/contracts
 cp artifacts/contracts/interfaces/**/*.json build/src/abi/interfaces
-
+echo "artifacts fin"
 rsync -av --include '*/*.sol' --exclude 'test' --exclude 'build' --exclude  'README.md' --exclude 'tools' contracts/ build/src
+echo "rsync fin"
 rm build/src/abi/contracts/*.dbg.json
 rm build/src/abi/interfaces/*.dbg.json
-
+echo "start ts node"
 npx ts-node scripts/publish/abi-export.ts
 npx ts-node scripts/publish/package.ts
 
