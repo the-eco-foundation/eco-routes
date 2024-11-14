a6b6c8f9520f5ccd4fdee52109774a048561e2c9
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2024-11-14 15:38:34 -0500

    enable all chains

--- scripts/deploy/ci.ts
+++ scripts/deploy/ci.ts
@@ -2,17 +2,17 @@ import { execSync } from 'child_process'
 
 process.env.DEPLOY_CI = 'true'
 const mainnetDep = [
-  // 'deployBase',
-  // 'deployOptimism',
-  // 'deployArbitrum',
+  'deployBase',
+  'deployOptimism',
+  'deployArbitrum',
   'deployMantle',
 ]
 const sepoliaDep = mainnetDep.map((dep) => dep + 'Sepolia')
 
 export function deployContracts() {
-  // for (const dep of mainnetDep) {
-  //   callYarnCmd(dep)
-  // }
+  for (const dep of mainnetDep) {
+    callYarnCmd(dep)
+  }
   for (const dep of sepoliaDep) {
     callYarnCmd(dep)
   }
