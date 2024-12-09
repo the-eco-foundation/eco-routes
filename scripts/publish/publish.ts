fbe1d564bb0da08cca3b02220031f58481172299
Stoyan Dimitrov <s.dimitrov19@gmail.com>
2024-12-09 16:21:52 -0500

    adding tag setting

--- scripts/publish/publish.ts
+++ scripts/publish/publish.ts
@@ -2,15 +2,18 @@
 
 import { execCMD } from "../utils"
 import { ProtocolVersion } from "../viem_deploy/ProtocolVersion"
+import { setGithubOutput } from "./set-github-output"
 
 export type PublishTag = 'beta' | 'latest' | 'rc'
-
+// GITHUB_REF=refs/tags/v0.0.509-beta GITHUB_OUTPUT=$(pwd)/a.txt npx tsx scripts/publish/publish.ts
 function publishTag() {
   const pv = new ProtocolVersion()
   // execCMD(`cd build && pwd`)
   // execCMD(`npm publish --tag ${pv.getReleaseTag()} --access public`)
   // console.log(`TAG=${pv.getReleaseTag()}`)
-  execCMD(`echo ${pv.getReleaseTag()}`)
+  // execCMD(`echo ${pv.getReleaseTag()}`)
+  // console.log(pv.getReleaseTag())
+  setGithubOutput('NPM_TAG', pv.getReleaseTag())
 }
 
 
