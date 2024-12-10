47d057f758c080a33f5ece46f7e2da5d25c4d654
John Whitton <jwhitton@eco.com>
2024-12-10 00:06:44 -0800

    RLP audit testing

---
+++ contracts/test/SimpleStorage.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.8.0;
+
+contract SimpleStorage {
+    uint256 public slot0Uint256 = 42;
+    address public slot1Address = 0x5FB30336A8d0841cf15d452afA297cB6D10877D7;
+    bytes32 public slot2Bytes32 = 0x825d2d3c51ea0ebdf25199ebb3e9d5835b6d61c407ef388ae4cde1f592306c95;
+    bytes32 public slot3Bytes32 = 0x000000000000000000000000000000000000000407ef388ae4cde1f592306c95;
+}
