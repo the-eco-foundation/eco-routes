fddfdacdb4b8e0dcc28969669bec6912e9f2a1b7
John Whitton <jwhitton@eco.com>
2024-12-10 17:05:00 -0800

    Address feedback from review

--- contracts/Prover.sol
+++ contracts/Prover.sol
@@ -32,7 +32,7 @@ contract Prover is SimpleProver {
     // Output slot for the game status (fixed)
     uint256 public constant L2_FAULT_DISPUTE_GAME_STATUS_SLOT = 0;
 
-    // Number of blocks to wait before Settlemnent Layer can be proven again
+    // Number of blocks to wait before Settlement Layer can be proven again
     uint256 public constant SETTLEMENT_BLOCKS_DELAY = 5;
 
     // This contract lives on an L2 and contains the data for the 'current' L1 block.
@@ -112,9 +112,9 @@ contract Prover is SimpleProver {
     /**
      * @notice emitted on a proving state if the blockNumber is less than or equal to the current blockNumber + SETTLEMENT_BLOCKS_DELAY
      * @param _inputBlockNumber the block number we are trying to prove
-     * @param _neededBlockNumber the  next block number that can been proven
+     * @param _nextProvableBlockNumber the next block number that can be proven
      */
-    error NeedLaterBlock(uint256 _inputBlockNumber, uint256 _neededBlockNumber);
+    error NeedLaterBlock(uint256 _inputBlockNumber, uint256 _nextProvableBlockNumber);
 
     /**
      * @notice emitted on a proving state if the blockNumber is less than or equal to the current blockNumber
@@ -297,11 +297,11 @@ contract Prover is SimpleProver {
             stateRoot: bytes32(RLPReader.readBytes(RLPReader.readList(rlpEncodedBlockData)[3]))
         });
         BlockProof memory existingBlockProof = provenStates[settlementChainId];
-        if ((existingBlockProof.blockNumber + SETTLEMENT_BLOCKS_DELAY) < blockProof.blockNumber) {
+        if (existingBlockProof.blockNumber + SETTLEMENT_BLOCKS_DELAY < blockProof.blockNumber) {
             provenStates[settlementChainId] = blockProof;
             emit L1WorldStateProven(blockProof.blockNumber, blockProof.stateRoot);
         } else {
-            revert NeedLaterBlock(blockProof.blockNumber, (existingBlockProof.blockNumber + SETTLEMENT_BLOCKS_DELAY));
+            revert NeedLaterBlock(blockProof.blockNumber, existingBlockProof.blockNumber + SETTLEMENT_BLOCKS_DELAY);
         }
     }
     /**
